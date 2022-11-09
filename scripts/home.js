c3b6f44749ff9327c57f063a588c417e4812c364
shrodinger <doremon98tybg@gmail.com>
2022-11-09 22:23:34 +0700

    code hom nay

--- scripts/home.js
+++ scripts/home.js
@@ -1,1 +1,28 @@
-'use strict'
+"use strict";
+
+const loginModal = document.getElementById("login-modal");
+const mainContent = document.getElementById("main-content");
+const logoutBtn = document.getElementById("btn-logout");
+const message = document.getElementById("welcome-message");
+homeDisplay();
+function homeDisplay() {
+  if (currentUser) {
+    loginModal.style.display = "none";
+    mainContent.style.display = "block";
+    message.textContent = `welcome ${currentUser.firstName}`;
+  } else {
+    loginModal.style.display = "block";
+    mainContent.style.display = "none";
+  }
+}
+//Hàm log out
+logoutBtn.addEventListener("click", function () {
+  const logout = confirm("Are you sure to Logout?");
+  if (logout) {
+    // localStorage.removeItem("currentUser");
+    currentUser = null;
+    saveToStorage("currentUser", currentUser);
+    homeDisplay();
+    console.log("đã logout");
+  }
+});
--- scripts/login.js
+++ scripts/login.js
@@ -3,4 +3,39 @@ const loginName = document.getElementById("input-username");
 const loginPassword = document.getElementById("input-password");
 const loginBtn = document.getElementById("btn-submit");
 
-loginBtn.addEventListener("click", function () {});
+loginBtn.addEventListener("click", function () {
+  const validate = validateData();
+  if (validate) {
+    let temp = 0;
+    for (let i = 0; i < userArr.length; i++) {
+      if (
+        //check user và pass
+        userArr[i].username === loginName.value &&
+        userArr[i].password === loginPassword.value
+      ) {
+        saveToStorage("currentUser", userArr[i]);
+        alert("Login Succesful!");
+
+        temp++;
+        //chuyến đến trang index.html nếu đăng nhập thành công
+        window.location.href = "../index.html";
+      }
+    }
+    if (temp === 0) {
+      alert("User name hoặc password không đúng!");
+    }
+  }
+});
+//Hàm kiểm tra tính hợp lệ của username và password
+function validateData() {
+  let check = true;
+  if (loginName.value === "") {
+    check = false;
+    alert("Please input User Name!");
+  }
+  if (loginPassword.value === "") {
+    check = false;
+    alert("Please input Password!");
+  }
+  return check;
+}
